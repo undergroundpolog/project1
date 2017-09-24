47569f7a62f0a0bc396daf5b23866fbbc7ee4ea3
Your Name <you@example.com>
2017-09-24 13:24:40 +0200

    Fix : can't create too mnay threads

--- source/MongoLoader.py
+++ source/MongoLoader.py
@@ -8,6 +8,7 @@ from pymongo import MongoClient
 from fileutils.Config import Config
 import threading
 import sys
+from time import sleep
 
 
 class MongoWriterThread(threading.Thread):
@@ -19,6 +20,7 @@ class MongoWriterThread(threading.Thread):
         
     def run(self):
         try:
+            sleep(10)
             self.db.turtle.insert_many(self.dictionaries)
         except:
             print("Error trying to save the document to MongoDB:", sys.exc_info()[0])
@@ -43,12 +45,19 @@ class MongoLoader(object):
         self.db.prefix.insert_one(prefixs)
     
     def loadTuplesToMongo(self,rdfDict):
+        sys.stdout.write("Number of threads: " + str(threading.active_count())+"\r")
+        sys.stdout.flush()
         thread = MongoWriterThread(self.db,rdfDict)
-        thread.start()
+        try:
+            thread.start()
+        except threading.ThreadError as e:
+            if str(e) == "can't start new thread":
+                actualThreadActiveCount = threading.activeCount()
+                while threading.activeCount() >= actualThreadActiveCount:
+                    print "Waiting a few seconds before trying again"
+                    sleep(7)
+                
+                return self.loadTuplesToMongo(rdfDict)
+            else:
+                raise
         return thread
-#     def loadTuplesToMongo(self,rdfDict):
-#         try:
-#             self.db.turtle.insert_many(rdfDict)
-#         except:
-#             print("Error trying to save the document to MongoDB:", sys.exc_info()[0])
-#             sys.exit()
